56d42aef358827727fbf34c6fbe6c5948564302c
hjorthjort <rikard.hjort@gmail.com>
2016-04-14 17:13:10 +0200

    Improved gitinspector

--- gitinspector.rb
+++ gitinspector.rb
@@ -1,7 +1,3 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Gitinspector < Formula
   desc ""
   homepage ""
@@ -9,23 +5,9 @@ class Gitinspector < Formula
   version "0.4.4"
   sha256 "c7a50772bf675d41b7c19508d8f2ea26725ce4ae16aba709d5b42db42e3d2459"
 
-  # depends_on "cmake" => :build
-  depends_on "python" # if your formula requires any X11/XQuartz components
-
   def install
-      bin.install_symlink "gitinspector.py"
-  end
+    libexec.install Dir["*"]
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test gitinspector`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bin.install_symlink libexec/"gitinspector/gitinspector.py"
   end
 end
